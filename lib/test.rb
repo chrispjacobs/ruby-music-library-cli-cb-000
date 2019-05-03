1ce3f62f871f9a0e184bfa0065d981153ac565d8
Christopher Jacobs <chrisj.3222@gmail.com>
2019-05-03 21:14:58 +0000

    Automatically backed up by Learn

--- lib/test.rb
+++ lib/test.rb
@@ -1,21 +1,21 @@
-require 'bundler'
-Bundler.require
-
-module Concerns
-end
-
-require_all 'lib'
-
-# # beneath this we have our test code, dawg
-
-Song.create_from_filename("Real Estate - Wonder Years - dream pop.mp3")
-
-#   # allow(music_library_controller).to receive(:gets).and_return("Real Estate")
-#   #
-#   # expect($stdout).to receive(:puts).with("Please enter the name of an artist:")
-#   # expect($stdout).to receive(:puts).with("1. Green Aisles - country")
-#   # expect($stdout).to receive(:puts).with("2. It's Real - hip-hop")
-#   # expect($stdout).to receive(:puts).with("3. Wonder Years - dream pop")
+# require 'bundler'
+# Bundler.require
 #
-mlc = MusicLibraryController.new
-puts mlc.play_song
+# module Concerns
+# end
+#
+# require_all 'lib'
+#
+# # # beneath this we have our test code, dawg
+#
+# Song.create_from_filename("Real Estate - Wonder Years - dream pop.mp3")
+#
+# #   # allow(music_library_controller).to receive(:gets).and_return("Real Estate")
+# #   #
+# #   # expect($stdout).to receive(:puts).with("Please enter the name of an artist:")
+# #   # expect($stdout).to receive(:puts).with("1. Green Aisles - country")
+# #   # expect($stdout).to receive(:puts).with("2. It's Real - hip-hop")
+# #   # expect($stdout).to receive(:puts).with("3. Wonder Years - dream pop")
+# #
+# mlc = MusicLibraryController.new
+# puts mlc.play_song
