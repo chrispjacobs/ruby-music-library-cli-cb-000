3e828a6ad63e0d98f706ea4c9f971821dd987f1e
Christopher Jacobs <chrisj.3222@gmail.com>
2019-05-03 20:39:26 +0000

    Automatically backed up by Learn

--- lib/test.rb
+++ lib/test.rb
@@ -1,15 +1,15 @@
-# require 'bundler'
-# Bundler.require
-#
-# module Concerns
-# end
-#
-# require_all 'lib'
-#
+require 'bundler'
+Bundler.require
+
+module Concerns
+end
+
+require_all 'lib'
+
 # # beneath this we have our test code, dawg
-#
-# Song.create_from_filename("Real Estate - Wonder Years - dream pop.mp3")
-#
+
+Song.create_from_filename("Real Estate - Wonder Years - dream pop.mp3")
+
 #   # allow(music_library_controller).to receive(:gets).and_return("Real Estate")
 #   #
 #   # expect($stdout).to receive(:puts).with("Please enter the name of an artist:")
@@ -17,5 +17,5 @@
 #   # expect($stdout).to receive(:puts).with("2. It's Real - hip-hop")
 #   # expect($stdout).to receive(:puts).with("3. Wonder Years - dream pop")
 #
-# mlc = MusicLibraryController.new
-# mlc.list_songs_by_artist
+mlc = MusicLibraryController.new
+mlc.play_song
